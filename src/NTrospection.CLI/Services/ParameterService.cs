f1b40ad136e55fb9a182a4bb6544b8cde3c27a98
WMaxZimmerman <william.m.zimmerman@gmail.com>
2019-06-04 21:50:56 -0500

    added GetAliasString To parameter service

--- src/NTrospection.CLI/Services/ParameterService.cs
+++ src/NTrospection.CLI/Services/ParameterService.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using NTrospection.CLI.Core;
+using NTrospection.CLI.Attributes;
 
 namespace NTrospection.CLI.Services
 {
@@ -33,5 +36,61 @@ namespace NTrospection.CLI.Services
         {
             return typeof(IEnumerable).IsAssignableFrom(type) && type.Name != "String";
         }
+
+	public string GetAliasString(ParameterInfo pi)
+	{
+	    var aliasString = "";
+	    var aliasChar = pi.GetCustomAttribute<CliParameter>()?.Alias;
+	    if (aliasChar != null && aliasChar != default(char)) aliasString = $" | {Settings.ArgumentPrefix}{aliasChar}";
+
+	    return aliasString;
+	}
+
+	// public List<string> GetParameterDocumentation(ParameterInfo cp)
+        // {
+	//     var output = new List<string>();
+        //     var priorityString = cp.HasDefaultValue ? "Optional" : "Required";
+        //     if (cp.HasDefaultValue && Settings.ParamDetail == "detailed") priorityString += $" with a default value of {cp.DefaultValue}";
+        //     var type = cp.ParameterType;
+        //     var isEnumerable = IsEnumerable(type);
+        //     if (isEnumerable)
+        //     {
+        //         if (type.GetGenericArguments().Length <= 0)
+        //         {
+        //             type = type.GetElementType();
+        //         }
+        //         else
+        //         {
+        //             type = type.GenericTypeArguments[0];
+        //         }
+        //     }
+
+        //     type = Nullable.GetUnderlyingType(type) ?? type;
+
+        //     var typeString = $"{(isEnumerable ? "List of " : "")}{type.Name}";
+        //     var aliasString = "";
+        //     var descripitionString = "";
+        //     if (cp.GetCustomAttribute<CliParameter>() != null)
+        //     {
+        //         if (cp.GetCustomAttribute<CliParameter>().Alias != default(char)) aliasString = $" | {Settings.ArgumentPrefix}{cp.GetCustomAttribute<CliParameter>().Alias}";
+        //         if (cp.GetCustomAttribute<CliParameter>().Description != null) descripitionString = $"Description: {cp.GetCustomAttribute<CliParameter>().Description}";
+        //     }
+        //     var docString = $"{Settings.ArgumentPrefix}{cp.Name}{aliasString} ({typeString}): This parameter is {priorityString}";
+
+        //     if (type.IsEnum)
+        //     {
+        //         var names = type.GetEnumNames();
+        //         docString += $" and must be {(isEnumerable ? "a collection of " : "")}one of the following ({string.Join(", ", names)}).";
+        //     }
+        //     else
+        //     {
+        //         docString += ".";
+        //     }
+
+        //     output.Add(docString);
+        //     if (descripitionString != "") output.Add(descripitionString);
+	    
+	//     return output;
+        // }
     }
 }
--- src/Tests/NTrospection.Tests.CLI/Services/ParameterServiceTests.cs
+++ src/Tests/NTrospection.Tests.CLI/Services/ParameterServiceTests.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NTrospection.CLI.Attributes;
+using NTrospection.CLI.Core;
 using NTrospection.CLI.Services;
 using NTrospection.Tests.CLI.App.Models;
 
@@ -9,9 +12,20 @@ namespace NTrospection.Tests.CLI.Models
     [TestClass]
     public class ParameterServiceTests
     {
+	const char _expectedAlias = 'b';
+	
 	[TestInitialize]
 	public void Init()
 	{
+	}
+
+	// === Fake Method For Reflection ===
+	public void FakeMethod(
+			       [CliParameter("sets foo")]int foo,
+			       [CliParameter(_expectedAlias, "sets bar")]int bar,
+			       string foobar)
+	{
+	    
 	}
 
 	[TestMethod]
@@ -57,6 +71,46 @@ namespace NTrospection.Tests.CLI.Models
 	    var actual = service.IsEnumerable(type);
 
 	    Assert.AreEqual(false, actual);
+	}
+
+	[TestMethod]
+	public void GetAliasString_ReturnsEmptyString_WhenPassedNoAlias()
+	{
+	    var service = new ParameterService();
+	    var pi = typeof(ParameterServiceTests)
+		.GetMethod("FakeMethod")
+		.GetParameters()[0];
+	    
+	    var actual = service.GetAliasString(pi);
+
+	    Assert.AreEqual("", actual);
+	}
+
+	[TestMethod]
+	public void GetAliasString_ReturnsEmptyString_WhenPassedParameterWithoutAttribute()
+	{
+	    var service = new ParameterService();
+	    var pi = typeof(ParameterServiceTests)
+		.GetMethod("FakeMethod")
+		.GetParameters()[2];
+	    
+	    var actual = service.GetAliasString(pi);
+
+	    Assert.AreEqual("", actual);
+	}
+
+	[TestMethod]
+	public void GetAliasString_ReturnsString_WhenPassedAlias()
+	{
+	    var service = new ParameterService();
+	    var pi = typeof(ParameterServiceTests)
+		.GetMethod("FakeMethod")
+		.GetParameters()[1];
+	    
+	    var actual = service.GetAliasString(pi);
+	    var expected = $" | {Settings.ArgumentPrefix}{_expectedAlias}";
+
+	    Assert.AreEqual(expected, actual);
 	}
     }
 }
