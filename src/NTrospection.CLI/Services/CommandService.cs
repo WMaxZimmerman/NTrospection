fec4d86edd0c7f1c656046ee274928522af53a1d
WMaxZimmerman <william.m.zimmerman@gmail.com>
2019-06-10 22:16:54 -0500

    Added sometests and updated sln to point to case sensitive paths

--- src/NTrospection.CLI/Services/CommandService.cs
+++ src/NTrospection.CLI/Services/CommandService.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using NTrospection.CLI.Attributes;
 using NTrospection.CLI.Models;
 
@@ -21,5 +23,130 @@ namespace NTrospection.CLI.Services
 
             return attribute?.Name;
 	}
+
+	public List<string> GetParameterErrors(CommandMethod command, List<CommandLineArgument> args)
+	{
+	    var errors = new List<string>();
+	    var arguments = args.Where(a => command.Info.GetParameters()
+				       .All(p => p.Name != a.Command && p.GetCustomAttribute<CliParameter>()?.Alias.ToString() != a.Command));
+	    
+	    foreach (var argument in arguments)
+            {
+                errors.Add($"The parameter '{argument.Command}' is not a valid parameter");
+            }
+
+	    return errors;
+	}
+
+	public bool ParameterMatchesArg(CommandMethod command, ParameterInfo pi, CommandLineArgument arg)
+	{
+	    var isPresent = true;
+	    
+	    if (arg.Command.ToLower() != pi.Name.ToLower())
+	    {
+		var paramAttribute = pi.GetCustomAttribute<CliParameter>();
+		if (paramAttribute != null)
+		{
+		    if (paramAttribute.Alias.ToString().ToLower() != arg.Command.ToLower()) isPresent = false;
+		}
+		else
+		{
+		    isPresent = false;
+		}
+	    }
+	    
+	    return isPresent;
+	}
+
+	// public void SetParameters(List<CommandLineArgument> args)
+        // {
+        //     var methodParams = new MethodParameters();
+
+        //     foreach (var argument in args.Where(a => Info.GetParameters().All(p => p.Name != a.Command && p.GetCustomAttribute<CliParameter>()?.Alias.ToString() != a.Command)))
+        //     {
+        //         methodParams.Errors.Add($"The parameter '{argument.Command}' is not a valid parameter.");
+        //     }
+
+        //     foreach (var parameter in Info.GetParameters())
+        //     {
+        //         var wasFound = false;
+        //         foreach (var argument in args)
+        //         {
+        //             if (argument.Command.ToLower() != parameter.Name.ToLower())
+        //             {
+        //                 var paramAttribute = parameter.GetCustomAttribute<CliParameter>();
+        //                 if (paramAttribute != null)
+        //                 {
+        //                     if (paramAttribute.Alias.ToString().ToLower() != argument.Command.ToLower()) continue;
+        //                 }
+        //                 else
+        //                 {
+        //                     continue;
+        //                 }
+        //             }
+        //             wasFound = true;
+
+        //             var type = parameter.ParameterType;
+
+        //             if (typeof(IEnumerable).IsAssignableFrom(type) && type.Name != "String")
+        //             {
+        //                 if (type.GetGenericArguments().Length <= 0)
+        //                 {
+        //                     var underType = type.GetElementType();
+        //                     var listType = typeof(List<>).MakeGenericType(underType);
+        //                     var list = (IList)Activator.CreateInstance(listType);
+
+        //                     foreach (var value in argument.Values)
+        //                     {
+        //                         list.Add(_parameterService.GetParamValue(value, underType));
+        //                     }
+
+        //                     var array = Array.CreateInstance(underType, list.Count);
+        //                     list.CopyTo(array, 0);
+
+        //                     methodParams.Parameters.Add(array);
+        //                 }
+        //                 else
+        //                 {
+        //                     var underType = type.GenericTypeArguments[0];
+        //                     var listType = typeof(List<>).MakeGenericType(underType);
+        //                     var list = (IList)Activator.CreateInstance(listType);
+
+        //                     foreach (var value in argument.Values)
+        //                     {
+        //                         list.Add(_parameterService.GetParamValue(value, underType));
+        //                     }
+
+        //                     methodParams.Parameters.Add(list);
+        //                 }
+        //             }
+        //             else if (type.Name == "Boolean")
+        //             {
+        //                 if (argument.Values.Count <= 0) argument.Values.Add("true");
+        //                 dynamic val = _parameterService.GetParamValue(argument.Values[0], parameter.ParameterType);
+        //                 methodParams.Parameters.Add(val);
+        //             }
+        //             else
+        //             {
+        //                 dynamic val = _parameterService.GetParamValue(argument.Values[0], parameter.ParameterType);
+        //                 methodParams.Parameters.Add(val);
+        //             }
+        //         }
+
+        //         if (!wasFound)
+        //         {
+        //             if (parameter.HasDefaultValue)
+        //             {
+        //                 methodParams.Parameters.Add(parameter.DefaultValue);
+        //             }
+        //             else
+        //             {
+        //                 methodParams.Errors.Add($"The parameter '{parameter.Name}' must be specified.");
+        //             }
+        //         }
+        //     }
+
+        //     Parameters = methodParams;
+        // }
     }
 }
--- src/Tests/NTrospection.Tests.CLI/Services/CommandServiceTests.cs
+++ src/Tests/NTrospection.Tests.CLI/Services/CommandServiceTests.cs
@@ -27,7 +27,10 @@ namespace NTrospection.Tests.CLI.Models
 
 	// === Fake Method For Reflection ===
 	[CliCommand("Fake", "Fake Method For Reflection")]
-	public void FakeMethod()
+	public void FakeMethod(
+			       [CliParameter('f', "")]int foo,
+			       string bar,
+			       [CliParameter("")]int foobar)
 	{
 	    
 	}
@@ -52,6 +55,111 @@ namespace NTrospection.Tests.CLI.Models
 	{
 	    string expected = null;
 	    var actual = _service.GetCommandName(_nonCommand);
+
+	    Assert.AreEqual(expected, actual);
+	}
+
+	[TestMethod]
+	public void GetParameterErrors_ReturnsEmptyList_WhenParamtersAreValid()
+	{
+	    var args = new List<CommandLineArgument>
+	    {
+		new CommandLineArgument
+		{
+		    Command = "foo",
+		    Value = "5",
+		    Order = 1,
+		},
+		new CommandLineArgument
+		{
+		    Command = "bar",
+		    Value = "something",
+		    Order = 2,
+		}
+	    };
+
+	    var expected = new List<string>();
+	    var actual = _service.GetParameterErrors(_command, args);
+
+	    CollectionAssert.AreEqual(expected, actual);
+	}
+
+	[TestMethod]
+	public void GetParameterErrors_ReturnsListOfErrors_WhenParamtersAreInvalid()
+	{
+	    var args = new List<CommandLineArgument>
+	    {
+		new CommandLineArgument
+		{
+		    Command = "fool",
+		    Value = "5",
+		    Order = 1,
+		},
+		new CommandLineArgument
+		{
+		    Command = "blah",
+		    Value = "something",
+		    Order = 2,
+		}
+	    };
+
+	    var expected = new List<string>
+	    {
+		"The parameter 'fool' is not a valid parameter",
+		"The parameter 'blah' is not a valid parameter"
+	    };
+	    var actual = _service.GetParameterErrors(_command, args);
+
+	    CollectionAssert.AreEqual(expected, actual);
+	}
+
+	[TestMethod]
+	public void ParameterMatchesArg_ReturnsTrue_WhenArgumentMatchesParameter()
+	{
+	    var arg = new CommandLineArgument
+	    {
+		Command = "foo",
+		Value = "5",
+		Order = 1,
+	    };
+	    var pi = _command.Info.GetParameters()[0];
+
+	    var expected = true;
+	    var actual = _service.ParameterMatchesArg(_command, pi, arg);
+
+	    Assert.AreEqual(expected, actual);
+	}
+
+	[TestMethod]
+	public void ParameterMatchesArg_ReturnsTrue_WhenArgumentMatchesParameterAlias()
+	{
+	    var arg = new CommandLineArgument
+	    {
+		Command = "f",
+		Value = "5",
+		Order = 1,
+	    };
+	    var pi = _command.Info.GetParameters()[0];
+
+	    var expected = true;
+	    var actual = _service.ParameterMatchesArg(_command, pi, arg);
+
+	    Assert.AreEqual(expected, actual);
+	}
+
+	[TestMethod]
+	public void ParameterMatchesArg_ReturnsFalse_WhenArgumentDoesNotMatchParameter()
+	{
+	    var arg = new CommandLineArgument
+	    {
+		Command = "barf",
+		Value = "something",
+		Order = 1,
+	    };
+	    var pi = _command.Info.GetParameters()[1];
+
+	    var expected = false;
+	    var actual = _service.ParameterMatchesArg(_command, pi, arg);
 
 	    Assert.AreEqual(expected, actual);
 	}
